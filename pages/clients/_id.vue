315732f28932de861c8374fb98addcfc86444a89
Bondok6 <kirolousbondok6@gmail.com>
2022-05-23 02:27:44 +0200

    Add Result to client

--- pages/clients/_id.vue
+++ pages/clients/_id.vue
@@ -13,39 +13,45 @@
       :modalTrigger="modalTrigger"
       @update:modalTrigger="toggleModal"
     >
-      <el-form class="p-5 d-flex flex-column">
-        <el-form-item label=" ">
+      <el-form
+        class="p-5 d-flex flex-column"
+        :rules="resultFormRules"
+        :model="resultForm"
+        ref="resultForm"
+      >
+        <el-form-item label=" " prop="titleAr">
           <span> اسم التحليل باللغة العربية </span>
           <el-input
-            v-model="input"
+            v-model="resultForm.titleAr"
             placeholder="اكتب اسم التحليل باللغة العربية"
           ></el-input>
         </el-form-item>
 
-        <el-form-item label=" ">
+        <el-form-item label=" " prop="titleEn">
           <span>اسم التحليل باللغة الانجليزية</span>
           <el-input
-            v-model="input"
+            v-model="resultForm.titleEn"
             placeholder="اكتب اسم التحليل باللغة الانجليزية"
           ></el-input>
         </el-form-item>
 
-        <el-form-item label=" ">
-          <span>اختر التحليل</span>
-          <el-upload
-            class="upload-demo"
-            action="https://jsonplaceholder.typicode.com/posts/"
-            :on-change="handleChange"
-            :file-list="fileList"
-          >
-            <el-button size="small" type="primary" class="secondary-btn"
-              >Click to upload</el-button
-            >
-          </el-upload>
+        <el-form-item label=" " prop="attachment">
+          <label for="formFile" class="form-label">اختر نتيجة التحليل</label>
+          <input
+            class="form-control"
+            type="file"
+            id="formFile"
+            @change="onFileSelected"
+            accept=""
+          />
         </el-form-item>
 
-        <button type="submit" class="secondary-btn w-50 align-self-end">
-          اضافة عميل
+        <button
+          type="submit"
+          class="secondary-btn w-50 align-self-end"
+          @click.prevent="addResult"
+        >
+          اضافة نتيجة
         </button>
       </el-form>
     </UIPopupForm>
@@ -70,10 +76,13 @@ export default {
   data() {
     return {
       modalTrigger: false,
-      addAdsForm: {},
-      addAdsFormRules: {
-        textareaAr: [{ required: true, message: "This Field Is Required" }],
-        textareaEn: [{ required: true, message: "This Field Is Required" }],
+      resultForm: {
+        attachment: null,
+      },
+      resultFormRules: {
+        titleAr: [{ required: true, message: "This Field Is Required" }],
+        titleEn: [{ required: true, message: "This Field Is Required" }],
+        attachment: [{ required: true, message: "This Field Is Required" }],
       },
       client: null,
     };
@@ -85,12 +94,53 @@ export default {
     toggleModal() {
       this.modalTrigger = !this.modalTrigger;
     },
+    onFileSelected(e) {
+      if (e.target.files.length > 0) {
+        this.resultForm.attachment = e.target.files[0];
+      }
+    },
     async getClient() {
       const clientRed = await this.$axios.get(
         `/fetch/${this.$route.params.id}/client`
       );
       this.client = await clientRed.data;
       console.log(this.client);
+    },
+    addResult() {
+      this.$refs.resultForm.validate(async (valid) => {
+        if (valid) {
+          const loading = this.$loading({
+            lock: true,
+            text: "Loading",
+            spinner: "el-icon-loading",
+            background: "rgba(0, 0, 0, 0.7)",
+          });
+          try {
+            const formData = new FormData();
+            formData.append("pdf", this.resultForm.attachment);
+            const fileRes = await this.$axios.post("/images", formData);
+            const attachment = await fileRes.data;
+
+            const fd = new FormData();
+            fd.append("titleAr", this.resultForm.titleAr);
+            fd.append("titleEn", this.resultForm.titleEn);
+            fd.append("attachment", attachment);
+            fd.append("subject", this.$route.params.id);
+            fd.append("subjectType", "user");
+
+            await this.$axios.post("/results", fd);
+
+            // Reset
+            this.resultForm = {};
+            this.toggleModal();
+            // await this.fetchOneClient();
+          } catch (error) {
+            console.log(error);
+          } finally {
+            loading.close();
+          }
+        }
+      });
     },
   },
 };
