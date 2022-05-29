488fb536bbbd9042d4f937407a0e53eed8438ae6
Bondok6 <kirolousbondok6@gmail.com>
2022-05-30 00:38:08 +0200

    Finish Analysis

--- pages/analysis/index.vue
+++ pages/analysis/index.vue
@@ -20,7 +20,7 @@
         ref="analysisForm"
       >
         <div class="row">
-          <el-form-item label=" " prop="image" class="col-lg-4 col-md-12">
+          <el-form-item label=" " prop="icon" class="col-lg-4 col-md-12">
             <label for="formFile" class="form-label"
               >أضف الايقون التي تعبر عن التحليل</label
             >
@@ -61,12 +61,17 @@
 
             <el-form-item class="col-lg-12">
               <span>اختر الفروع التي ينتمى اليها</span>
-              <el-select v-model="caseInput" placeholder="الفروع" class="w-100">
+              <el-select
+                v-model="analysisForm.branches"
+                placeholder="الفروع"
+                class="w-100"
+                multiple
+              >
                 <el-option
-                  v-for="item in options"
-                  :key="item.value"
-                  :label="item.label"
-                  :value="item.value"
+                  v-for="branch in allBranches"
+                  :key="branch.id"
+                  :label="branch.titleAr"
+                  :value="branch.id"
                 >
                 </el-option>
               </el-select>
@@ -102,63 +107,83 @@
               class="w-100"
             >
               <el-option
-                v-for="item in options"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value"
+                v-for="(location, index) in locations"
+                :key="index"
+                :label="location"
+                :value="location"
               >
               </el-option>
             </el-select>
           </el-form-item>
         </div>
         <div class="row">
-          <el-form-item label=" " class="col-lg-4 col-md-12">
+          <el-form-item
+            label=" "
+            class="col-lg-4 col-md-12"
+            prop="possibleResults"
+          >
             <span>النسبة المرتفعة</span>
             <el-input
-              v-model="input"
+              v-model="analysisForm.possibleResults[0].descriptionAr"
               placeholder="اكتب النسبة المناسبة"
             ></el-input>
           </el-form-item>
 
-          <el-form-item label=" " class="col-lg-4 col-md-12">
+          <el-form-item
+            label=" "
+            class="col-lg-4 col-md-12"
+            prop="possibleResults"
+          >
             <span>النسبة المتوسطة</span>
             <el-input
-              v-model="input"
+              v-model="analysisForm.possibleResults[1].descriptionAr"
               placeholder="اكتب النسبة المناسبة"
             ></el-input>
           </el-form-item>
 
-          <el-form-item label=" " class="col-lg-4 col-md-12">
+          <el-form-item
+            label=" "
+            class="col-lg-4 col-md-12"
+            prop="possibleResults"
+          >
             <span>النسبة المنخفضة</span>
             <el-input
-              v-model="input"
+              v-model="analysisForm.possibleResults[2].descriptionAr"
               placeholder="اكتب النسبة المناسبة"
             ></el-input>
           </el-form-item>
         </div>
         <div class="row">
-          <el-form-item label=" " class="col-lg-4 col-md-12">
+          <el-form-item label=" " class="col-lg-4 col-md-12" prop="measuresAr">
             <span>الإجراءات الازمة باللغة العربية</span>
             <el-input
-              v-model="input"
+              v-model="analysisForm.measuresAr"
               placeholder="اكتب الإجراءات الازمة باللغة العربية"
             ></el-input>
           </el-form-item>
         </div>
         <div class="row">
-          <el-form-item label=" " class="col-lg-4 col-md-12">
+          <el-form-item label=" " class="col-lg-4 col-md-12" prop="measuresEn">
             <span>الإجراءات الازمة باللغة الانجليزية</span>
             <el-input
-              v-model="input"
+              v-model="analysisForm.measuresEn"
               placeholder="اكتب الإجراءات الازمة باللغة الانجليزية"
             ></el-input>
           </el-form-item>
         </div>
         <div class="buttons w-100 p-3 d-flex gap-2 justify-content-end">
-          <button type="submit" class="secondary-btn w-25 align-self-end">
+          <button
+            type="submit"
+            class="secondary-btn w-25 align-self-end"
+            @click.prevent="addTest"
+          >
             حفظ
           </button>
-          <button type="submit" class="secondary-btn w-25 align-self-end">
+          <button
+            type="submit"
+            class="secondary-btn w-25 align-self-end"
+            @click.prevent="cancel"
+          >
             الغاء
           </button>
         </div>
@@ -254,28 +279,63 @@ export default {
   data() {
     return {
       modalTrigger: false,
-      input: "",
+      selectedImage: null,
+      selectedImageUrl: null,
       searchInput: "",
       selectedImage: null,
       selectedImageUrl: null,
       analysisForm: {
-        image: null,
+        icon: null,
+        possibleResults: [
+          {
+            titleAr: "النسبة المرتفعة",
+            titleEn: "High rate",
+            descriptionEn: "test",
+          },
+          {
+            titleAr: "النسبة المتوسطة",
+            titleEn: "Mid rate",
+            descriptionEn: "test",
+          },
+          {
+            titleAr: "النسبة المنخفضة",
+            titleEn: "Low rate",
+            descriptionEn: "test",
+          },
+        ],
       },
       analysisFormRules: {
         titleAr: [{ required: true, message: "Arabic title Is Required" }],
         titleEn: [{ required: true, message: "English title Is Required" }],
+        availableAt: [{ required: true, message: "Location Is Required" }],
+        branches: [{ required: true, message: "Branches Is Required" }],
+        icon: [{ required: true, message: "Icon Is Required" }],
+        measuresAr: [{ required: true, message: "This field Is Required" }],
+        measuresEn: [{ required: true, message: "This field Is Required" }],
+        possibleResults: [
+          { required: true, message: "This fields Is Required" },
+        ],
       },
       page: 1,
       totalPages: 1,
       tests: [],
+      allBranches: [],
+      locations: ["both", "home", "lab"],
     };
   },
   async fetch() {
     await this.getTests();
+    await this.getAllBranches();
   },
   methods: {
     toggleModal() {
       this.modalTrigger = !this.modalTrigger;
+    },
+    onImageSeclected(e) {
+      if (e.target.files.length > 0) {
+        this.selectedImage = this.analysisForm.icon = e.target.files[0];
+        this.selectedImageUrl = URL.createObjectURL(this.selectedImage);
+      }
     },
     async getTests() {
       let params = { page: this.page };
@@ -308,6 +368,133 @@ export default {
             message: "Delete canceled",
           });
         });
+    },
+    async getAllBranches() {
+      const branchesRes = await this.$axios.get("/branches");
+      this.allBranches = await branchesRes.data.docs;
+    },
+    addTest() {
+      this.$refs.analysisForm.validate(async (valid) => {
+        if (valid) {
+          if (!this.selectedImage) return;
+          const loading = this.$loading({
+            lock: true,
+            text: "Loading",
+            spinner: "el-icon-loading",
+            background: "rgba(0, 0, 0, 0.7)",
+          });
+          try {
+            const fd = new FormData();
+            fd.append("titleAr", this.analysisForm.titleAr);
+            fd.append("titleEn", this.analysisForm.titleEn);
+            fd.append("icon", this.analysisForm.icon);
+            fd.append("availableAt", this.analysisForm.availableAt);
+            this.analysisForm.branches.forEach((branch) => {
+              fd.append("branches", branch);
+            });
+            fd.append("measuresAr", this.analysisForm.measuresAr);
+            fd.append("measuresEn", this.analysisForm.measuresEn);
+            fd.append("price", this.analysisForm.price || "");
+            fd.append(
+              "possibleResults[0][descriptionAr]",
+              this.analysisForm.possibleResults[0].descriptionAr
+            );
+            fd.append(
+              "possibleResults[1][descriptionAr]",
+              this.analysisForm.possibleResults[1].descriptionAr
+            );
+            fd.append(
+              "possibleResults[2][descriptionAr]",
+              this.analysisForm.possibleResults[2].descriptionAr
+            );
+            fd.append(
+              "possibleResults[0][titleAr]",
+              this.analysisForm.possibleResults[0].titleAr
+            );
+            fd.append(
+              "possibleResults[1][titleAr]",
+              this.analysisForm.possibleResults[1].titleAr
+            );
+            fd.append(
+              "possibleResults[2][titleAr]",
+              this.analysisForm.possibleResults[2].titleAr
+            );
+            fd.append(
+              "possibleResults[0][titleEn]",
+              this.analysisForm.possibleResults[0].titleEn
+            );
+            fd.append(
+              "possibleResults[1][titleEn]",
+              this.analysisForm.possibleResults[1].titleEn
+            );
+            fd.append(
+              "possibleResults[2][titleEn]",
+              this.analysisForm.possibleResults[2].titleEn
+            );
+            fd.append("descriptionAr", "تحليل");
+            fd.append("descriptionEn", "analysis");
+            fd.append(
+              "priceAfterDiscount",
+              this.analysisForm.priceAfterDiscount || ""
+            );
+            await this.$axios.post("/add-tests", fd);
+            // Reset
+            this.analysisForm = {
+              icon: null,
+              possibleResults: [
+                {
+                  titleAr: "النسبة المرتفعة",
+                  titleEn: "High rate",
+                  descriptionEn: "test",
+                },
+                {
+                  titleAr: "النسبة المتوسطة",
+                  titleEn: "Mid rate",
+                  descriptionEn: "test",
+                },
+                {
+                  titleAr: "النسبة المنخفضة",
+                  titleEn: "Low rate",
+                  descriptionEn: "test",
+                },
+              ],
+            };
+            this.selectedImage = null;
+            this.selectedImageUrl = null;
+            this.toggleModal();
+            await this.getTests();
+          } catch (error) {
+            console.log(error);
+          } finally {
+            loading.close();
+          }
+        }
+      });
+    },
+    cancel() {
+      this.analysisForm = {
+        icon: null,
+        possibleResults: [
+          {
+            titleAr: "النسبة المرتفعة",
+            titleEn: "High rate",
+            descriptionEn: "test",
+          },
+          {
+            titleAr: "النسبة المتوسطة",
+            titleEn: "Mid rate",
+            descriptionEn: "test",
+          },
+          {
+            titleAr: "النسبة المنخفضة",
+            titleEn: "Low rate",
+            descriptionEn: "test",
+          },
+        ],
+      };
+      this.selectedImage = null;
+      this.selectedImageUrl = null;
+      this.toggleModal();
     },
   },
   computed: {
